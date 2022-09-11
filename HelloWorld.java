302a795a99a6894df56c9cde186f880ceb022a09
Chuchumbrik <mussha2010@yandex.ru>
2022-09-11 15:12:44 +0300

    ну что, первый коммит так сказать с первой программой на java

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Бля, первая программа, hello world и сразу работает, пизданутся");
+    }
+}
